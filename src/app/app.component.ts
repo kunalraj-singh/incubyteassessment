f374bca9b01c699b3e93d71dfd1883d5105d35de
kunalraj-singh <kunal.kumar668@gmail.com>
2024-08-21 11:42:23 +0530

    Removed unused dependencies
    Removed unused dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FormsModule, CommonModule, FormControl],
+  imports: [RouterOutlet,FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
